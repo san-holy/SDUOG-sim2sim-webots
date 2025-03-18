1ecec6d0f203b85a0abce137ee020e3d74142ecb
san-holy <1328856429@qq.com>
2025-03-18 21:09:37 +0800

    include/controller_listener.h

---
+++ include/controller_listener.h
@@ -0,0 +1,18 @@
+#ifndef controller_listener_h
+#define controller_listener_h
+
+#include <lcm/lcm-cpp.hpp>
+#include <iostream>
+#include "deploy/deploy_lcm/msg_lcm/robot_control/exlcm/example_t.hpp"  // 引入头文件
+
+
+class GamepadHandler {
+    public:
+        float speed_x;
+        float speed_y;  
+        float yaw;
+        float height;
+        void handleMessage(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const exlcm::example_t* msg);
+    };
+
+#endif
